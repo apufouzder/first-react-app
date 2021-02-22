4bf87f4219524217ace3b96f82480f8fa4e4dfcf
apufouzder <apufouzdar@gmail.com>
2021-02-23 00:39:54 +0600

    pass dynamic data to componenets and props

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ function App() {
     names: "Ratan",
     job: "online"
   }
-  const parson2 = {
-    names: "Pranto",
-    job: "student"
-  }
+  
   const style ={
     backgroundColor: 'red',
     color: 'blue',
     padding: '20px'
   }
-  const parson3 = {
-    names: 'Kamal',
-    salary: 2000
-  }
+  
+  const myArray = ['apu', 'pranto', 'ratan', 'kamal']
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello</p>
-        <h1 style={{color: 'cyan', backgroundColor: 'orange', padding: '10px'}}>Name: {parson.names}</h1>
-        <h2 className="" style={style}>Another Person: {parson2.names}  Jos: {parson2.job}</h2>
-        <h3 style={{color: 'orange', backgroundColor: 'white'}}>Parson3: {parson3.names}, Salary: {parson3.salary}</h3>
+        <Person name={myArray[0]}></Person>
+        <Person name={myArray[2]}></Person>
+        <Person name='Pranto' job='Football'></Person>
+        <Person name='Ratan' job='Online'></Person>
       </header>
     </div>
   );
+}
+
+
+function Person(props){
+  return(
+    <div style={{border: '2px solid gold', margin: '10px', padding: '10px', width: '400px'}}>
+      <h1>Name: {props.name}</h1>
+      <h3>Profession: {props.job}</h3>
+    </div>
+  )
 }
 
 export default App;
