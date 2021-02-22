5ff56582e18de597218d0d6fca5d630ada132a80
apufouzder <apufouzdar@gmail.com>
2021-02-23 01:25:58 +0600

    object to components and access object

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const parson = {
-    names: "Ratan",
-    job: "online"
-  }
-  
-  const style ={
-    backgroundColor: 'red',
-    color: 'blue',
-    padding: '20px'
-  }
-  
   const myArray = ['apu', 'pranto', 'ratan', 'kamal']
+  const products = [
+    { name: 'Photoshop', price: '$99.99', pra: 'Create precise designs, Illustrations, and vector graphics on desktop and iPad.' },
+    { name: 'Acrobat Pro', price: '$55.99', pra: 'Create precise designs, Illustrations, and vector graphics on desktop and iPad.' },
+    { name: 'Premiere Pro', price: '$49.99', pra: 'Create precise designs, Illustrations, and vector graphics on desktop and iPad.' },
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+
         <Person name={myArray[0]}></Person>
         <Person name={myArray[2]}></Person>
         <Person name='Pranto' job='Football'></Person>
@@ -28,10 +25,32 @@ function App() {
   );
 }
 
+function Product(props) {
+  const productStyle = {
+    width: '400px',
+    height: '280px',
+    border: '2px solid cyan',
+    margin: '5px',
+    float: 'left',
+    padding: '5px',
+    color: 'cyan',
+    background: '#182338'
+  }
+  const { name, price, pra } = props.product;
+  console.log(props);
+  return (
+    <div style={productStyle}>
+      <h4>{name}</h4>
+      <h2>{price}</h2>
+      <p>{pra}</p>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 
-function Person(props){
-  return(
-    <div style={{border: '2px solid gold', margin: '10px', padding: '10px', width: '400px'}}>
+function Person(props) {
+  return (
+    <div style={{ border: '2px solid gold', margin: '10px', padding: '10px', width: '400px' }}>
       <h1>Name: {props.name}</h1>
       <h3>Profession: {props.job}</h3>
     </div>
