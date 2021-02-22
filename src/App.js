01aafe3fb9384f0246a9145a13c7974f13c3a12e
apufouzder <apufouzdar@gmail.com>
2021-02-23 00:02:21 +0600

    style and html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const parson = {
+    names: "Ratan",
+    job: "online"
+  }
+  const parson2 = {
+    names: "Pranto",
+    job: "student"
+  }
+  const style ={
+    backgroundColor: 'red',
+    color: 'blue',
+    padding: '20px'
+  }
+  const parson3 = {
+    names: 'Kamal',
+    salary: 2000
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +28,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>hello</p>
+        <h1 style={{color: 'cyan', backgroundColor: 'orange', padding: '10px'}}>Name: {parson.names}</h1>
+        <h2 className="" style={style}>Another Person: {parson2.names}  Jos: {parson2.job}</h2>
+        <h3 style={{color: 'orange', backgroundColor: 'white'}}>Parson3: {parson3.names}, Salary: {parson3.salary}</h3>
       </header>
     </div>
   );
